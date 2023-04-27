cb862abd314c1ccdafdfedba273ae08a5ea7387c
root <root@vps.server.local>
2023-04-27 20:54:58 +0000

    something git push discoveryrepo Mcgarvey1000

--- backend/server.js
+++ backend/server.js
@@ -55,7 +55,7 @@ mongoose.connect(
 
    app.get('*', (req, res) =>
    //  res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-   	res.sendFile(path.resolve(__dirname,'/frontend/build/index.html'))
+   	res.sendFile(path.join(__dirname,'/frontend/build/index.html'))
    )
  } else {
    app.get('/', (req, res) => {
